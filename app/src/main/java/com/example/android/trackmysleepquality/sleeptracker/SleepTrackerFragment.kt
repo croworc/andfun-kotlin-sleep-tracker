4ecfb9577039ed256e481fed4da68c71050f95c3
ikrotzky <ikr@croworc.com>
2020-06-24 10:18:32 +0200

    [Step.04-Ex] refactor: Replace deprecated call ViewModelProviders.of() w/ constructor call

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -22,7 +22,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.databinding.FragmentSleepTrackerBinding
@@ -57,7 +57,7 @@ class SleepTrackerFragment : Fragment() {
 
         //COMPLETED (01) Update onCreateView() to get an instance of SleepTrackerViewModel
         //using the factory.
-        val sleepTrackerViewModel = ViewModelProviders.of(this, viewModelFactory)
+        val sleepTrackerViewModel = ViewModelProvider(this, viewModelFactory)
                 .get(SleepTrackerViewModel::class.java)
 
         //COMPLETED (02) Set this fragment as the lifecycle owner of the binding.
